be63929839ed6a4ae0280d40be119c4901f9b3a8
MertAYHAN <mertayhan123123@gmail.com>
2024-10-26 16:29:34 +0300

    26.10

--- client/app/login/page.tsx
+++ client/app/login/page.tsx
@@ -6,7 +6,7 @@ import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 export default async function Home() {
   const session = await getServerSession(authOptions);
 
-  if (session) redirect("/login");
+  if (session) redirect("/main");
   return (
     
    <LoginForm />
